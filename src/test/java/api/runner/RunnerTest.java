5e9a6dc1b91b6befd667a9cac15401482e3553b3
bedarevaolga <olh4bedareva@yandex.by>
2021-09-29 21:19:42 +0300

    tag runnerApi

--- src/test/java/api/runner/RunnerTest.java
+++ src/test/java/api/runner/RunnerTest.java
@@ -2,6 +2,7 @@ package api.runner;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.jupiter.api.Tag;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
@@ -10,5 +11,6 @@ import org.junit.runner.RunWith;
         glue = {"api.steps"},
         snippets = CucumberOptions.SnippetType.CAMELCASE
 )
+@Tag("RunnerApi")
 public class RunnerTest {
 }
